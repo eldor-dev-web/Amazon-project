3b45463d3bdf0b72e30b0127ea412f5e4e63d0e2
Eldor Ayimbetov <eldorayimbetov@gmail.com>
2026-08-17 15:37:54 +0500

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,7 @@ import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
-
-hello();
-
-const today = dayjs();
-const deliveryaDate = today.add(7, 'days');
-console.log(deliveryaDate.format('dddd, MMMM D')); 
+import { renderPaymentSummary } from './paymentSummary.js';
 
  export function renderOrderSummary() {
 let cartSummaryHTML = '';
@@ -129,6 +124,8 @@ document.querySelectorAll('.js-delete-link')
             `.js-cart-item-contener-${productId}`
         );
         container.remove();
+
+        renderPaymentSummary();
     });
   });
 
@@ -138,6 +135,7 @@ document.querySelectorAll('.js-delete-link')
             const {productId, deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId)
             renderOrderSummary(); 
+            renderPaymentSummary();
         });
       });
  }
